45ce3e674400b84e86e3cd9787cb1bce2e992819
Tracie35 <kariukitracie@gmail.com>
2022-09-13 10:23:41 +0300

    homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,12 @@ import Reviews from "./components/Reviews"
 import { Route, Routes } from "react-router-dom"
 import './App.css'
 import image1 from "./images/image1.jpg"
+import image2 from "./images/Alaska-Airlines.jpg"
+import image3 from "./images/American-Airlines.jpg"
+import image4 from "./images/Delta.jpg"
+import image5 from "./images/JetBlue.jpg"
+import image6 from "./images/Southwest-Airlines.jpg"
+import image7 from "./images/United-Airlines.jpg"
 import Hero from "./components/Hero"
 function App() {
   return (
